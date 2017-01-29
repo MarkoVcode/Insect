fdd2f297d6bf2da795c4298b44d8a90cb95459dc
MarkoV <marek.kaszewski@gmail.com>
2017-01-29 13:08:16 +0000

    adding license info

--- ins_websockets/src/main/java/org/scg/websockets/integration/MessagesListener.java
+++ ins_websockets/src/main/java/org/scg/websockets/integration/MessagesListener.java
@@ -1,3 +1,26 @@
+/**
+ *  Copyright 2015 MarkoV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * InsectIn.space
+ *
+ * Project home: https://github.com/MarkoVcode/Insect
+ *
+ * @build <BUILDTAG>
+ * @date <BUILDDATE>
+ * @version <RELEASEVERSION>
+ */
 package org.scg.websockets.integration;
 
 import fi.iki.elonen.NanoHTTPD;
@@ -9,13 +32,12 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 /**
- * Receives push messages - must be protected!! for local calls only no internet access
- * This to be implemented on web socket servlet?
+ * Receives push messages - must be secured for local calls only
  */
 
 /**
  * Created by developer on 10/05/16.
- * RESPONSIBILITY: Receive HTTP requests from API with message or unsubscribe request
+ * RESPONSIBILITY: Receive HTTP requests from API with message
  */
 public class MessagesListener extends NanoHTTPD implements Runnable {
 
