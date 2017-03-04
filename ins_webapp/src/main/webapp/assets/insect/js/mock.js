7806467214ab24e05b246c71832937dbe9981ac8
MarkoV <marek.kaszewski@gmail.com>
2017-03-04 00:07:36 +0000

    Mocking service UI improvement

--- ins_webapp/src/main/webapp/assets/insect/js/mock.js
+++ ins_webapp/src/main/webapp/assets/insect/js/mock.js
@@ -13,4 +13,33 @@ $(document).ready(function(){
             }
         });
     });
+
+    $('.mock-add-resource-button').click(function(e){
+        var group = $( this ).data("mock-group");
+        var template = Handlebars.compile(getTemplate("#mock-resource-template"));
+        var data = {group: group,
+                    tid: Math.random().toString(36).substring(7)};
+        $("#mock-methods-container-"+group).append(template(data));
+        $('.mock-add-method-button').click(function(e){
+            var group = $( this ).data("mock-group");
+            var template = Handlebars.compile(getTemplate("#mock-method-template"));
+            var data = {group: group,
+                        tid: Math.random().toString(36).substring(7)};
+            $("#mock-methods-"+group).append(template(data));
+            $('.mock-delete-method-button').click(function(e){
+                var group = $( this ).data("mock-group");
+                $("#mock-method-container-"+group).remove();
+            });
+        });
+        $('.mock-delete-resource-button').click(function(e){
+            var group = $( this ).data("mock-group");
+            $("#mock-resource-container-"+group).remove();
+        });
+    });
+
+    function getTemplate(template) {
+        var source  = $(template).html();
+        var sourcep = source.replace(new RegExp("<%", 'g'), "{{");
+        return sourcep.replace(new RegExp("%>", 'g'), "}}");
+    }
 });
